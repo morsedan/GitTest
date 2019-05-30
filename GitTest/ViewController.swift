8574a4c77c789934f685b9caa89363cfe6525134
Dan Morse <morsedan@outlook.com>
2019-05-29 19:34:48 -0700

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by morse on 5/29/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
